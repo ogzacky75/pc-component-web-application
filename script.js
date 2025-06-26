6d9402838819537dc49958f0c604331ef85de4db
ogzacky75 <ogzacky75@gmail.com>
2025-06-27 01:20:37 +0300

    adding dark mode

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 const darkmode = document.getElementById('darkmode')
 const submitbtn = document.getElementById('submit')
 const resetbtn = document.getElementById('reset')
+const formDiv = document.getElementsByClassName('.form-div')
+const finalTest = document.querySelector('.final-test')
 
 darkmode.addEventListener('click', ()=>{
-	darkmode.onclick()
+	document.body.classList.toggle('dark-mode')
+	document.body.style.transition='1s'
+	finalTest.classList.toggle('dark-mode')
+
 	
 })
 
