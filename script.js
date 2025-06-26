479236e35dcff434167559a27d7b47d2b2758298
ogzacky75 <ogzacky75@gmail.com>
2025-06-26 17:43:28 +0300

    adding java and adding transition on divs

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+const darkmode = document.getElementById('darkmode')
+const submitbtn = document.getElementById('submit')
+const resetbtn = document.getElementById('reset')
 
+darkmode.addEventListener('click', ()=>{
+	darkmode.onclick()
+	
+})
+
+submitbtn.addEventListener('click', ()=>{
+	darkmode.onclick()
+
+})
+resetbtn.addEventListener('click', ()=>{
+	darkmode.onclick()
+	
+})
